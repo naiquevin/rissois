3a6c7b1c1c8a2a00e671f7443ad3dbbd546ab151
Vineet Naik <naikvin@gmail.com>
2023-10-09 12:08:40 +0530

    Add structure and handle errors in cli processing
    Defined an 'execute' method for the `Cli` struct which actually
    matches the args and runs the code. It returns a result type. For now,
    we're returning a tuple as part of 'Ok' and a '&str' as part of 'Err'.
    
    The 'main' function calls the 'execute' method and based on the
    returned value, it either exits with exit code 0 (success) or
    1 (failure). In case of failure, the error string is printed to stderr
    using the 'eprintln!' macro.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::io;
+use std::process;
 use clap::{Parser, Subcommand};
 
 mod indent;
@@ -38,21 +39,40 @@ struct Cli {
     command: Option<Commands>,
 }
 
+fn indent(stdin: bool) -> Result<(), String> {
+    if stdin {
+        let input_lines = stdin_to_vec();
+        let output_lines = indent::hard_indent_org(&input_lines);
+        vec_to_stdout(output_lines);
+        Ok(())
+    } else {
+        let errmsg = String::from("File support not implemented. Please use --stdin for now");
+        Err(errmsg)
+    }
+}
+
+impl Cli {
+
+    fn execute(&self) -> Result<(), String> {
+        match self.command {
+            Some(Commands::Indent { stdin }) => indent(stdin),
+            None => {
+                let errmsg = String::from("Please specify the subcommand");
+                Err(errmsg)
+            }
+        }
+    }
+}
+
 fn main() {
     let cli = Cli::parse();
+    let result = cli.execute();
 
-    match &cli.command {
-        Some(Commands::Indent { stdin }) => {
-            if *stdin {
-                let input_lines = stdin_to_vec();
-                let output_lines = indent::hard_indent_org(&input_lines);
-                vec_to_stdout(output_lines);
-            } else {
-                println!("File support not implemented. Please use --stdin for now")
-            }
-        },
-        None => {
-            println!("Please specify the subcommand")
+    match result {
+        Ok(()) => process::exit(0),
+        Err(e) => {
+            eprintln!("Error {}", e);
+            process::exit(1)
         }
     }
 }
